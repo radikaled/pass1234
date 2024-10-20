f976303ab7ec79ed7c7add6e705f037e65b3e249
Neal Kim <neal.kim@mymail.champlain.edu>
2024-10-20 04:54:05 -0700

    Add method to generate encrypted symmetric key with AES-256-CBC
    - Implemented `generate_encrypted_symmetric_key()` that returns a Base64-encoded encrypted symmetric key and IV.
    - Used AES-256-CBC for encryption with a 256-bit key and 128-bit IV.
    - Added comments for using os.urandom() as a CSPRNG substitute.

--- app/util/secure.py
+++ app/util/secure.py
@@ -1,20 +1,24 @@
 import os
+import base64
 
-from cryptography.hazmat.primitives import hashes, hmac
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDFExpand
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.exceptions import InvalidKey
 
 class KeyController:
     def __init__(self, email: str, password: str):
         self.iterations = 600000
-        self.length = 32  # 256 bits = 32 bytes
+        self.length = 32  # 256-bits
         self._email = email
         self._password = password
         self.master_key = self._generate_master_key()
         self.stretched_master_key = self._generate_stretched_master_key()
         self.master_password_hash = self._generate_master_password_hash()
 
+    # Generate the master key
+    # Use the email address for salt
     def _generate_master_key(self) -> bytes:
         
         kdf = PBKDF2HMAC(
@@ -22,25 +26,30 @@ class KeyController:
             length=self.length,
             salt=self._email.encode(),
             iterations=self.iterations
-            )
+        )
         
         master_key = kdf.derive(self._password.encode())
 
         return master_key
 
+    # Stretches the master key to 512-bits
+    # The main reason for stretching is to increase entropy
+    # We could also generate and store the salt but it is technically
+    # unnecessary
     def _generate_stretched_master_key(self) -> bytes:
 
-        hkdf = HKDF(
+        hkdf = HKDFExpand(
             algorithm=hashes.SHA256(),
-            length=64,  # 512 bits = 64 bytes
-            salt=None,
-            info=None
-            )
+            length=64,  # 512-bits
+            info=b'encryption_key'  #  Additional context about the key
+        )
         
         stretched_master_key = hkdf.derive(self.master_key)
 
         return stretched_master_key
 
+    # Generate the master password hash
+    # Use the password itself as the salt
     def _generate_master_password_hash(self) -> bytes:
             
         kdf = PBKDF2HMAC(
@@ -48,23 +57,44 @@ class KeyController:
             length=self.length,
             salt=self._password.encode(),
             iterations=self.iterations
-            )
+        )
         
         master_password_hash = kdf.derive(self.master_key)
 
         return master_password_hash
 
-    def valid_master_password_hash(self, hash: str) -> bool:
+    def verify_master_password_hash(self, hash: bytes) -> bool:
         
         kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             length=self.length,
             salt=self._password.encode(),
             iterations=self.iterations
-            )
+        )
         
         try:
-            kdf.verify(self.master_key, bytes.fromhex(hash))
+            kdf.verify(self.master_key, hash)
             return True
         except InvalidKey:
             return False
+        
+    def generate_encrypted_symmetric_key(self) -> tuple[str, str]:
+        encryption_key = self.master_key[:32] # Use first 256-bits
+        
+        # In the real world one would use a 
+        # Cryptographically Secure Pseudorandom Number Generator (CSPRNG)
+        # but os.urandom() is the next best thing.
+        # https://cryptography.io/en/latest/random-numbers/
+        symmetric_key = os.urandom(64) # 512-bits
+        iv = os.urandom(16) # 128-bits
+        
+        # AES-256-CBC (Cipher Block Chaining)
+        cipher = Cipher(algorithms.AES(encryption_key), modes.CBC(iv))
+        encryptor = cipher.encryptor()
+        protected_key = encryptor.update(symmetric_key) + encryptor.finalize()
+        
+        # Encode the protected key and iv with base64
+        b64_protected_key = base64.b64encode(protected_key).decode('utf-8')
+        b64_iv = base64.b64encode(iv).decode('utf-8')
+        
+        return b64_protected_key, b64_iv
