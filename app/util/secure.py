cd1b11843795923066facb2b73326aa7725328e5
Neal Kim <neal.kim@mymail.champlain.edu>
2024-10-20 09:33:38 -0700

    Implement "encrypt-then-MAC"

--- app/util/secure.py
+++ app/util/secure.py
@@ -1,12 +1,19 @@
 import os
 
-from cryptography.hazmat.primitives import hashes
+from typing import NamedTuple
+from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.kdf.hkdf import HKDFExpand
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.exceptions import InvalidKey
 
 class KeyController:
+    # Keep things nice and neat while preserving type hints
+    class ProtectedKeyArtifacts(NamedTuple):
+        iv: bytes
+        protected_key: bytes
+        hmac_signature: bytes
+    
     def __init__(self, email: str, password: str):
         self.iterations = 600000
         self.length = 32  # 256-bits
@@ -77,8 +84,9 @@ class KeyController:
         except InvalidKey:
             return False
         
-    def generate_protected_symmetric_key(self) -> tuple[bytes, bytes]:
-        encryption_key = self._master_key[:32]  # Use first 256-bits
+    def generate_protected_symmetric_key(self) -> ProtectedKeyArtifacts:
+        encryption_key = self._stretched_master_key[:32]  # Use first 256-bits
+        hmac_key = self._stretched_master_key[:32]  # Use last 256-bits
         
         # In the real world one would use a 
         # Cryptographically Secure Pseudorandom Number Generator (CSPRNG)
@@ -86,10 +94,32 @@ class KeyController:
         # https://cryptography.io/en/latest/random-numbers/
         symmetric_key = os.urandom(64) # 512-bits
         iv = os.urandom(16) # 128-bits
-        
+
         # AES-256-CBC (Cipher Block Chaining)
-        cipher = Cipher(algorithms.AES(encryption_key), modes.CBC(iv))
-        encryptor = cipher.encryptor()
+        # "encrypt-then-MAC"
+        cipher_aes_cbc = Cipher(
+            algorithms.AES(encryption_key),
+            modes.CBC(iv)
+        )
+        encryptor = cipher_aes_cbc.encryptor()
+        # Encryption is a streaming operation
+        # always make sure to capture the value of update()!
+        # finalize() alone won't return the correct encrypted result!
         protected_key = encryptor.update(symmetric_key) + encryptor.finalize()
         
-        return protected_key, iv
+        # Generate hash-based message authentication codes (HMAC)
+        # Can be thought of as the "signature" to ensure integrity of the
+        # ciphertext (i.e. protected_key)
+        # https://cryptography.io/en/latest/hazmat/primitives/mac/hmac/
+        hmac_signer = hmac.HMAC(hmac_key, hashes.SHA256())
+        hmac_signer.update(iv)
+        hmac_signer.update(protected_key)
+        hmac_signature = hmac_signer.finalize()
+        
+        key_artifacts = self.ProtectedKeyArtifacts(
+            iv=iv,
+            protected_key=protected_key,
+            hmac_signature=hmac_signature
+        )
+        
+        return key_artifacts 
