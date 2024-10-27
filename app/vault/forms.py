61b1ecd785d10ae3c9b15274341772b06f9acf82
Neal Kim <neal.kim@mymail.champlain.edu>
2024-10-27 07:02:52 -0700

    Add CredentialUpdateForm

--- app/vault/forms.py
+++ app/vault/forms.py
@@ -2,8 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
-class CredentialForm(FlaskForm):
+class CredentialCreateForm(FlaskForm):
     name = StringField(validators=[DataRequired()])
     username = StringField(validators=[DataRequired()])
-    password = PasswordField(validators=[DataRequired()])
+    ciphertext = PasswordField(validators=[DataRequired()])
+    website = StringField(validators=[DataRequired()])
+
+class CredentialUpdateForm(FlaskForm):
+    name = StringField(validators=[DataRequired()])
+    username = StringField(validators=[DataRequired()])
+    ciphertext = StringField(validators=[DataRequired()])
     website = StringField(validators=[DataRequired()])
