c3730f3f74dcb6e57bcc3e697a72dd907bdeec1a
Neal Kim <neal.kim@mymail.champlain.edu>
2024-11-10 13:13:38 -0800

    Update attributes in User model

--- app/models/user.py
+++ app/models/user.py
@@ -12,6 +12,7 @@ from app.extensions import db
 class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str]
     master_password_hash: Mapped[str] = mapped_column(unique=True)
     date_created: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
