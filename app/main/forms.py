65cf8f75c760707028acc5414a42d9726449dba9
Neal Kim <neal.kim@mymail.champlain.edu>
2024-11-10 13:14:11 -0800

    Separate forms for registration and login

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,5 +3,10 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class UserForm(FlaskForm):
-    email = StringField('E-Mail', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField(validators=[DataRequired()])
+    name = StringField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired()])
+
+class LoginForm(FlaskForm):
+    email = StringField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired()])
