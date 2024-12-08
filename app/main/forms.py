6a6dbe06c865ab99b92779cf8ca5bec081ba1454
Neal Kim <neal.kim@mymail.champlain.edu>
2024-12-08 05:33:12 -0800

    Incorporate EqualTo validator

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 class UserForm(FlaskForm):
     email = StringField(validators=[DataRequired()])
     name = StringField(validators=[DataRequired()])
     password = PasswordField(validators=[DataRequired()])
+    password_verify = PasswordField(validators=[DataRequired(),
+        EqualTo('password', message='Passwords must match.')]
+    )
 
 class LoginForm(FlaskForm):
     email = StringField(validators=[DataRequired()])
